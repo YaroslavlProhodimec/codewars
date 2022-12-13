c8ddd77c764a01bd77224331fb991561c241c581
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2022-12-13 18:08:16 +0300

    Initial commit

---
+++ 01.js
@@ -0,0 +1,8 @@
+
+function billboard(name, price = 30){
+    let sum = 0;
+    for(i=0;i < name.length;  i++) {
+        sum += price
+    }
+    return sum
+}
---
+++ 02.js
@@ -0,0 +1,10 @@
+
+function century(year) {
+    let centuryCount = 0;
+    while (year > 0){
+        year = year - 100;
+        centuryCount = centuryCount + 1;
+    }
+    return centuryCount;
+    return;
+}
